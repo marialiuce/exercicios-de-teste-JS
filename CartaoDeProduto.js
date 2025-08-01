21bbfe1f003dab2b4a6d0feca21bb75aa5f89f15
Alice <malyefx@gmail.com>
2025-08-01 15:37:10 -0300

    9commit

---
+++ CartaoDeProduto.js
@@ -0,0 +1,20 @@
+function CartaoDeProduto() {
+  return (
+    <div>
+      <h3>Smartphone Modelo X</h3>
+      
+      <img 
+        className="imagem-produto"
+        alt="Smartphone Modelo X"
+        src="https://via.placeholder.com/150" 
+      />
+
+      <p className="preco">R$ 1.999,00</p>
+      
+      <p className="descricao">Um ótimo smartphone com a melhor câmera.</p>
+      
+      <button>Comprar</button>
+    </div>
+  );
+}
+
